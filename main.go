b63f41b802ba5b37def2452fcc5b9d8959d29a81
Swapan Shridhar <sshridhar@cloudera.com>
2020-03-03 22:30:26 -0800

    Modularization Stage 1

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	//"encoding/json"
 	"fmt"
 	"k8s.io/client-go/tools/clientcmd"
@@ -16,24 +17,21 @@ import (
 func main() {
 	fmt.Println("main()::RedHat Openshift APIs Demo ....")
 
-	fmt.Println("main()::=======================================================")
+	fmt.Println("main()::=======================================================\n")
 	// Instantiate loader for kubeconfig file.
-	fmt.Println("\n main()::Reading kubeconfig ..: ")
+	fmt.Println("main()::Reading kubeconfig ..: ")
 	kubeconfig := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(
 		clientcmd.NewDefaultClientConfigLoadingRules(),
 		&clientcmd.ConfigOverrides{},
 	)
-	fmt.Println("main()::Done reading kubeconfig ..: ")
-	fmt.Println(kubeconfig)
+	fmt.Println("main()::Done reading kubeconfig\n")
 
-	// Determine the Namespace referenced by the current context in the
-	// kubeconfig file.
+	// Determine the Namespace referenced by the current context in the kubeconfig file.
 	namespace, _, err := kubeconfig.Namespace()
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println("\nmain()::Current Namespace ..: ")
-	fmt.Println(namespace)
+	fmt.Printf("main()::Current Namespace : %s\n", namespace)
 
 	// Get a rest.Config from the kubeconfig file.  This will be passed into all
 	// the client objects we create.
@@ -41,7 +39,7 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println("\nmain()::Created clientConfig")
+	fmt.Println("main()::Created clientConfig\n")
 	fmt.Println(restconfig)
 
 	// Create a Kubernetes core/v1 client.
@@ -49,23 +47,8 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println("\nmain()::Created client")
-	fmt.Println(coreclient)
-	/*
-	//coreclient.Pods(namespace).Get("aa", metav1.GetOptions{})
+	fmt.Println("main()::Created client\n")
 
-	svcList, e := coreclient.ServiceAccounts(namespace).List(
-		metav1.ListOptions{},
-	)
-	if (e != nil) {
-		fmt.Println("Got error while listing Svc Account...")
-		panic(err)
-	}
-	fmt.Println("Listing Svc Accounts : ")
-	fmt.Println(svcList)
-	svcListMar, _ := json.Marshal(svcList)
-	fmt.Println(svcListMar)
-	*/
 	// INSTANTIATE THE TEMPLATE.
 
 	// 2. Create Secrets
@@ -76,26 +59,12 @@ func main() {
 
 	// 4. Create Svc Account
 	var svcAccName = "prog-sc"
-	var labels = map[string]string{
+	var svcAccLabels = map[string]string{
 		"key1": "value1",
 		"key2": "value2",
 	}
-	fmt.Println("\nmain()::Creating Svc Account")
 
-
-	svcAcc, err := coreclient.ServiceAccounts(namespace).Create(&corev1.ServiceAccount{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: svcAccName,
-			Namespace: namespace,
-			Labels: labels,
-		},
-	})
-	if err != nil {
-		fmt.Println("\nmain()::Received error while creating Svc Account.")
-		panic(err)
-	}
-	fmt.Println(svcAcc)
-	fmt.Println("\nmain():: Done. Returning ...")
+	createServiceAccount(coreclient, namespace, svcAccName, svcAccLabels)
 
 	/*
 		// To set Template parameters, create a Secret holding overridden parameters
@@ -118,4 +87,43 @@ func main() {
 	log.Fatal(http.ListenAndServe(":8081", nil))
 
 
+	fmt.Println("main():: Done. Returning ...")
+}
+
+// Create a Service Account
+func createServiceAccount(coreclient *corev1client.CoreV1Client, namespace string, svcAccName string, svcAccLabels map[string]string) {
+	fmt.Printf("main()::Creating Svc Account : %s", svcAccName)
+
+
+	svcAcc, err := coreclient.ServiceAccounts(namespace).Create(&corev1.ServiceAccount{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: svcAccName,
+			Namespace: namespace,
+			Labels: svcAccLabels,
+		},
+	})
+	if err != nil {
+		fmt.Println("\nmain()::Received error while creating Svc Account.")
+		panic(err)
+	}
+	fmt.Printf("main()::Created Svc Account : %s\n", svcAccName)
+	fmt.Println(svcAcc)
+}
+
+// Lists Service Accounts
+func listServiceAccounts(coreclient *corev1client.CoreV1Client, namespace string) {
+	//coreclient.Pods(namespace).Get("aa", metav1.GetOptions{})
+  fmt.Printf("Listing ServiceAccounts in NS : %s", namespace)
+
+	svcList, err := coreclient.ServiceAccounts(namespace).List(
+		metav1.ListOptions{},
+	)
+	if (err != nil) {
+		fmt.Println("main()::Got error while listing Svc Account...")
+		panic(err)
+	}
+	fmt.Println("Listing Svc Accounts : ")
+	fmt.Println(svcList)
+	svcListMar, _ := json.Marshal(svcList)
+	fmt.Println(svcListMar)
 }
